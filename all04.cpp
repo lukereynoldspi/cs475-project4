21d69f5163cb57ad4e5ad1fe27ad43b5ab091d07
lukereynoldspi <lukereynoldspi@gmail.com>
2023-05-14 22:18:22 -0700

    removed simd code

--- all04.cpp
+++ all04.cpp
@@ -113,31 +113,7 @@ main( int argc, char *argv[ ] )
 void
 NonSimdMul( float *A, float *B, float *C, int n )
 {
-	int limit = ( n/SSE_WIDTH ) * SSE_WIDTH;
-	__asm
-	(
-		".att_syntax\n\t"
-		"movq    -24(%rbp), %r8\n\t"		// a
-		"movq    -32(%rbp), %rcx\n\t"		// b
-		"movq    -40(%rbp), %rdx\n\t"		// c
-	);
-
-	for( int i = 0; i < limit; i += SSE_WIDTH )
-	{
-		__asm
-		(
-			".att_syntax\n\t"
-			"movups	(%r8), %xmm0\n\t"	// load the first sse register
-			"movups	(%rcx), %xmm1\n\t"	// load the second sse register
-			"mulps	%xmm1, %xmm0\n\t"	// do the multiply
-			"movups	%xmm0, (%rdx)\n\t"	// store the result
-			"addq $16, %r8\n\t"
-			"addq $16, %rcx\n\t"
-			"addq $16, %rdx\n\t"
-		);
-	}
-
-	for( int i = limit; i < len; i++ )
+	for( int i = 0; i < n; i++ )
 	{
 		C[i] = A[i] * B[i];
 	}
@@ -146,44 +122,14 @@ NonSimdMul( float *A, float *B, float *C, int n )
 float
 NonSimdMulSum( float *A, float *B, int n )
 {
-	float sum[4] = { 0., 0., 0., 0. };
-	int limit = ( n/SSE_WIDTH ) * SSE_WIDTH;
+	float sum = 0.0;
 
-	__asm
-	(
-		".att_syntax\n\t"
-		"movq    -40(%rbp), %r8\n\t"		// a
-		"movq    -48(%rbp), %rcx\n\t"		// b
-		"leaq    -32(%rbp), %rdx\n\t"		// &sum[0]
-		"movups	 (%rdx), %xmm2\n\t"		// 4 copies of 0. in xmm2
-	);
+    for( int i = 0; i < n; i++ )
+    {
+        sum += A[i] * B[i];
+    }
 
-	for( int i = 0; i < limit; i += SSE_WIDTH )
-	{
-		__asm
-		(
-			".att_syntax\n\t"
-			"movups	(%r8), %xmm0\n\t"	// load the first sse register
-			"movups	(%rcx), %xmm1\n\t"	// load the second sse register
-			"mulps	%xmm1, %xmm0\n\t"	// do the multiply
-			"addps	%xmm0, %xmm2\n\t"	// do the add
-			"addq $16, %r8\n\t"
-			"addq $16, %rcx\n\t"
-		);
-	}
-
-	__asm
-	(
-		".att_syntax\n\t"
-		"movups	 %xmm2, (%rdx)\n\t"	// copy the sums back to sum[ ]
-	);
-
-	for( int i = limit; i < len; i++ )
-	{
-		sum[0] += A[i] * B[i];
-	}
-
-	return sum[0] + sum[1] + sum[2] + sum[3];
+    return sum;
 }
 
 
